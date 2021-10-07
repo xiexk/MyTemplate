9694ec5614e29710d5aafb38e438929da2345c0f
GitHub Action <action@github.com>
2021-10-07 09:36:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/xiexk/mytemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xiexk/mytemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xiexk.mytemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xiexk.mytemplate.MyBundle
 
 class MyApplicationService {
 
